c6fb370b781da0c661d671bbb57fd6a5213da419
Biraj Mainali <birajmainali10@gmail.com>
2024-11-22 19:51:49 +0545

    ef core demo

---
+++ Entities/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace Efcore_demo.Entities;
+
+public class Contact
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
