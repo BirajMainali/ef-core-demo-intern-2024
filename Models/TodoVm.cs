0b2183cbd0d712f0d27d778e842d1710771174aa
Bishal <bishalshah431@gmail.com>
2024-11-25 18:11:15 +0545

    remove unnecessasy code like hiden taskdate

--- Models/TodoVm.cs
+++ Models/TodoVm.cs
@@ -2,7 +2,7 @@
 
 public class TodoVm
 {
-    public DateTime TaskDate { get; set; }
+    
     public string TaskTitle { get; set; }
     public string TaskDescription { get; set; }
     public bool Status { get; set; }
